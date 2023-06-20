0749cea897328d873cf142b2b49685101b356319
Taylor Lo <taylorloph@gmail.com>
2023-06-20 15:12:12 -0400

    Updated places-routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,27 @@
-const express = require('express')
+const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It works" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34 th StaticRange, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  res.json({place});
 });
 
 module.exports = router;
