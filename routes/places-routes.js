744dea891902fd21f88d5d311fcc265db1764bb3
Taylor Lo <taylorloph@gmail.com>
2023-06-20 15:23:43 -0400

    Added find place by id api

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -24,4 +24,12 @@ router.get("/:pid", (req, res, next) => {
   res.json({place});
 });
 
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    })
+    res.json({ place });
+})
+
 module.exports = router;
