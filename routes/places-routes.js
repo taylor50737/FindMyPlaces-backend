f447132fd5574ca536f3b234625b3dc8c96fe0d2
Taylor Lo <taylorloph@gmail.com>
2023-06-20 16:33:42 -0400

    Added Error Model

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -23,9 +25,7 @@ router.get("/:pid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided id.');
-    error.code = 404;
-    throw error;
+    throw new HttpError("Could not find a place for the provided id.", 404);
   }
   res.json({ place });
 });
@@ -36,9 +36,9 @@ router.get("/user/:uid", (req, res, next) => {
     return p.creator === userId;
   });
   if (!place) {
-    const error = new Error('Could not find a place for the provided id.');
-    error.code = 404;
-    return next(error);
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
   }
   res.json({ place });
 });
