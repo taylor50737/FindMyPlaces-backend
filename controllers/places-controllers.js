b97c3973caa440e0a9c26fde32f7975796c1e15a
Taylor Lo <taylorloph@gmail.com>
2023-06-21 10:25:59 -0400

    Added createPlace POST route

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,3 +1,5 @@
+const { v4: uuid } = require('uuid');
+
 const HttpError = require("../models/http-error");
 
 const DUMMY_PLACES = [
@@ -39,5 +41,22 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
