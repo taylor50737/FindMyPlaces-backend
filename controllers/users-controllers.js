dd0173856adbf7b0295875a5bbe728ec08140b90
Taylor Lo <taylorloph@gmail.com>
2023-06-21 15:08:16 -0400

    Setup user routes

---
+++ controllers/users-controllers.js
@@ -0,0 +1,52 @@
+const { v4: uuid } = require("uuid");
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Max Schwarz",
+    email: "test@test.com",
+    password: "testers",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = res.json;
+
+  const hasUser = DUMMY_USERS.find(u => u.email === email);
+  if (hasUser) {
+    throw new HttpError('Could not create user email already exists', 422)
+  }
+
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError(
+      "Could not identify user, credentials seem to be wrong.",
+      401
+    );
+  }
+  res.json({ message: "Logged in!" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
