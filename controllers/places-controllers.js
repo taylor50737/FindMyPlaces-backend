b57cc10a965a6834bbe6ee0801a180cf1f8bf480
Taylor Lo <taylorloph@gmail.com>
2023-06-21 11:39:18 -0400

    Added PATCH route for updating place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,4 @@
-const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const HttpError = require("../models/http-error");
 
@@ -57,6 +57,24 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId)};
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({place: updatedPlace});
+};
+
+const deletePlace = (req, res, next) => {};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const placesControllers = require('../controllers/places-controllers')
+const placesControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
 
@@ -8,6 +8,10 @@ router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
-router.post('/', placesControllers.createPlace);
+router.post("/", placesControllers.createPlace);
+
+router.patch("/:pid", placesControllers.updatePlace);
+
+router.delete("/:pid", placesControllers.deletePlace);
 
 module.exports = router;
