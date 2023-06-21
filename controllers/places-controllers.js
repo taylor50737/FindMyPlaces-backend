f9c61654094ffb045619ebb7b495c92650af1c1d
Taylor Lo <taylorloph@gmail.com>
2023-06-21 15:23:15 -0400

    Added Express-validator & places-routes validation

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const { v4: uuid } = require("uuid");
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -43,6 +44,11 @@ const getPlacesByUserId = (req, res, next) => {
 
 const createPlace = (req, res, next) => {
   const { title, description, coordinates, address, creator } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, please check your data.')
+  }
+  // const title = req.body.title;
   const createdPlace = {
     id: uuid(),
     title,
