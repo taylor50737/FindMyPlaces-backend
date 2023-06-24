d0307f3e47b0a0c801707a3722e19e7740e69163
Taylor Lo <taylorloph@gmail.com>
2023-06-23 22:23:12 -0400

    Updated getPlaceById function

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -19,16 +19,24 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+
+  let place;
+
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError('Something went wrong, could not find a place.', 500);
+    return next(error);
+  }
 
   if (!place) {
-    throw new HttpError('Could not find a place for the provided id.', 404);
+    const error = new HttpError('Could not find a place for the provided id.', 404);
+    return next(error);
   }
-  res.json({ place });
+
+  res.json({ place: place.toObject({ getters: true }) });
 };
 
 const getPlacesByUserId = (req, res, next) => {
