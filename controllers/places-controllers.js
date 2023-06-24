0760ceab8ea8b1eb0b725daa2f3b3813dd66f9e7
Taylor Lo <taylorloph@gmail.com>
2023-06-23 23:15:01 -0400

    Updated getPlacesByIserId

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -39,15 +39,22 @@ const getPlaceById = async (req, res, next) => {
   res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((p) => {
-    return p.creator === userId;
-  });
-  if (!places || places.length === 0) {
-    return next(new HttpError('Could not find a places for the provided user id.', 404));
+  let places;
+
+  try {
+    places = await Place.find({ creator: userId });
+  } catch (err) {
+    const error = new HttpError('Fetching places failed, please try again later', 500);
+    return next(error);
   }
-  res.json({ places });
+
+  if (!places || places.length === 0) {
+    const error = new HttpError('Could not find a place for the provided user id.', 500);
+    return next(error);
+  }
+  res.json({ places: places.map((place) => place.toObject({ getters: true })) });
 };
 
 const createPlace = async (req, res, next) => {
