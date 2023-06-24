204a98ca98c94639bb710b836b417ae5117c1f12
Taylor Lo <taylorloph@gmail.com>
2023-06-24 18:02:24 -0400

    Change database from places to mern

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'mongodb+srv://user:HyUoCqdBY2FBD2dy@cluster0.fn9cabk.mongodb.net/places?retryWrites=true&w=majority',
+    'mongodb+srv://user:HyUoCqdBY2FBD2dy@cluster0.fn9cabk.mongodb.net/mern?retryWrites=true&w=majority',
   )
   .then(() => {
     app.listen(8080);
