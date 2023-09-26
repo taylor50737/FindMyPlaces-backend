0bf7c9464b0cd99f15512f4cd1fd8ed77b35c946
Taylor Lo <taylorloph@gmail.com>
2023-09-26 12:15:16 -0400

    Added fileFilter

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -17,7 +17,12 @@ const fileUpload = multer({
       const ext = MIME_TYPE_MAP[file.mimetype];
       cb(null, uuid() + '.' + ext);
     },
-  })
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error('Invalid mime type!');
+    cb(error, isValid);
+  }
 });
 
 module.exports = fileUpload;
