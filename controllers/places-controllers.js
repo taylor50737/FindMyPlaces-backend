50484338f5b3eb1cd23f1fe687d717bf3bf9bc9c
Taylor Lo <taylorloph@gmail.com>
2023-09-26 15:15:08 -0400

    Updated new place photo

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -66,8 +66,7 @@ const createPlace = async (req, res, next) => {
     description,
     address,
     location: coordinates,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+    image: req.file.path,
     creator,
   });
 
