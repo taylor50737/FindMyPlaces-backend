377098f9c810ec590e7954e95b1075542eff44d5
Taylor Lo <taylorloph@gmail.com>
2023-09-26 13:18:10 -0400

    Fixed fileUpload middleware

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -11,7 +11,7 @@ const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'upload/images')
+      cb(null, 'uploads/images');
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
@@ -22,7 +22,7 @@ const fileUpload = multer({
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
     let error = isValid ? null : new Error('Invalid mime type!');
     cb(error, isValid);
-  }
+  },
 });
 
 module.exports = fileUpload;
