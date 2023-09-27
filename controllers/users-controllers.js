e1cf5a9ea4a9f48254a6d730bc45948b0c82d22f
Taylor Lo <taylorloph@gmail.com>
2023-09-27 10:55:10 -0400

    JWT generation

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -59,7 +60,21 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      'supersecret_dont_share',
+      {
+        expiresIn: '1h',
+      },
+    );
+  } catch (err) {
+    const error = new HttpError('Signing up failed, please try again later.', 500);
+    return next(error);
+  }
+
+  res.status(201).json({ user: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -94,7 +109,21 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  res.json({ message: 'Logged in!', user: existingUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      'supersecret_dont_share',
+      {
+        expiresIn: '1h',
+      },
+    );
+  } catch (err) {
+    const error = new HttpError('Logging in failed, please try again later.', 500);
+    return next(error);
+  }
+
+  res.json({ userId: existingUser.id, email: existingUser.email, token: token });
 };
 
 exports.getUsers = getUsers;
